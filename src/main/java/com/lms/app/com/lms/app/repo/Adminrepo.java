a24e841b7d83e8f8d0b070de68d7c81adf1db224
Sivasowmian <kirushanbalakrishnan@gmail.com>
2021-05-28 18:51:15 +0530

    05/28/2021 admin login change,feedback change

--- src/main/java/com/lms/app/com/lms/app/repo/Adminrepo.java
+++ src/main/java/com/lms/app/com/lms/app/repo/Adminrepo.java
@@ -11,11 +11,11 @@ public interface Adminrepo extends JpaRepository<Admin_account, Integer> {
 //	@Query(value = "SELECT * FROM admin_account WHERE Username =?1",nativeQuery = true)
 //	boolean findUser(String Username);
 
-	@Query(value = "select * from admin_account where id=?1",nativeQuery = true)
-	Admin_account findUser(String Username);
-
-
-
-//	@Query(value = "SELECT * FROM admin_account WHERE Username =?1",nativeQuery = true)
+//	@Query(value = "select * from admin_account where id=?1",nativeQuery = true)
 //	Admin_account findUser(String Username);
+
+
+
+	@Query(value = "SELECT * FROM admin_account WHERE Username =?1",nativeQuery = true)
+	Admin_account findUser(String Username);
 }
